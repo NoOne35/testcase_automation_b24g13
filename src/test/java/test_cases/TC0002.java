9595d331e65b99c8261273046f699b761626092c
hanss <hansemusa1@gmail.com>
2021-11-10 16:11:47 -0500

    Updated TC0002

--- src/test/java/test_cases/TC0002.java
+++ src/test/java/test_cases/TC0002.java
@@ -3,34 +3,33 @@ package test_cases;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 public class TC0002 {
+
     public static void main(String[] args) {
+
+        String url = "http://practice.cybertekschool.com/forgot_password";
+        String actualUsername = "helpdesk25@cybertekschool.com";
+        String loginButton = "form_submit";
+
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-
-    String url = "http://practice.cybertekschool.com/forgot_password";
         driver.get(url);
 
+        driver.findElement(By.name("email")).sendKeys(actualUsername);
 
-    String actualUsername = "helpdesk25@cybertekschool.com";
-    WebElement emailButton= driver.findElement(By.name("email"));
-        emailButton.sendKeys(actualUsername);
-        System.out.println("entered email");
-    String loginButton = "form_submit";
         driver.findElement(By.id(loginButton)).click();
-        System.out.println("clicked login button");
 
-    String expectedURL = "http://practice.cybertekschool.com/email_sent";
-    String actualURL = driver.getCurrentUrl();
+        String expectedURL = "http://practice.cybertekschool.com/email_sent";
+        String actualURL = driver.getCurrentUrl();
 
-    if (expectedURL.equals(actualURL)){
-        System.out.println("Displayed on Screen: Your e-mail's been sent!");
-    } else {
-        System.out.println("Fail - home page link does not match expected link");
-    }
+        if (expectedURL.equals(actualURL)){
+            System.out.println("Displayed on Screen: Your e-mail's been sent!");
+        } else {
+        System.out.println("FAIL: Home page link does not match expected link");
+
+        }
 }
 }
 
